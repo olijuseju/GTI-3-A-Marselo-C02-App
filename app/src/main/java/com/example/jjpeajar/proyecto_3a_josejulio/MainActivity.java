97adeb561bbe09fb740c378161e6287eed107d4c
Jose Julio <jjpeajar@epsg.upv.es>
2021-09-17 16:00:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jjpeajar.proyecto_3a_josejulio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
