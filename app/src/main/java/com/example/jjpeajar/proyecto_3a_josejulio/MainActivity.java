488b4b334c821d15eefbbf876d5f96fd00548308
Jose Julio <jjpeajar@epsg.upv.es>
2021-10-07 17:49:00 +0200

    Creacion de directorio y archivos

--- app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/MainActivity.java
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/MainActivity.java
@@ -225,7 +225,7 @@ public class MainActivity extends AppCompatActivity {
         //this.buscarEsteDispositivoBTLE( Utilidades.stringToUUID( "EPSG-GTI-PROY-3A" ) );
 
         //this.buscarEsteDispositivoBTLE( "EPSG-GTI-PROY-3A" );
-        this.buscarEsteDispositivoBTLE( "fistro" );
+        this.buscarEsteDispositivoBTLE( "AusiasBM-GTI" );
 
     } // ()
 
